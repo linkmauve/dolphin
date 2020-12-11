newperson1746/dolphin-wayland

ControllerInterface: Get window size from frontend

Source/Core/Core/Core.cpp
@@ -496,7 +496,8 @@ static void EmuThread(std::unique_ptr<BootParameters> boot, WindowSystemInfo wsi
   }
   else
   {
-    g_controller_interface.ChangeWindow(wsi.render_surface);
+    g_controller_interface.ChangeWindow(wsi.render_surface, wsi.render_surface_width,
+                                        wsi.render_surface_height);
     Pad::LoadConfig();
     Keyboard::LoadConfig();
   }

linkmauve/dolphin

Source/Core/Core/Core.cpp
@@ -458,7 +458,8 @@ static void EmuThread(std::unique_ptr<BootParameters> boot, WindowSystemInfo wsi
   }
   else
   {
-    g_controller_interface.ChangeWindow(wsi.render_window);
+    g_controller_interface.ChangeWindow(wsi.render_window, wsi.render_surface_width,
+                                        wsi.render_surface_height);
     Pad::LoadConfig();
     Keyboard::LoadConfig();
   }